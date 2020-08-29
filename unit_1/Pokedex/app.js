d5c3e526674cd358a833707dbf3988d28a92e90a
John Bruno <johnnyb@MacBook-Pro.local>
2020-08-28 20:31:42 -0500

    Final edit

--- unit_1/Pokedex/app.js
+++ unit_1/Pokedex/app.js
@@ -1,5 +1,4 @@
 const $pokemonImage = $('<img>')
-const $pictureBtn = $('.picture-btn')
 const $polaroid = $('.modal-polaroid')
 const $signature = $('.modal-polaroid-sig')
 
@@ -18,8 +17,7 @@ $('form').on('submit', (event) => {
             $('#id').html(data.id),
             $('#type').html(data.types.map( type => type.type.name).join(', ')),
             $pokemonImage.attr("src", data.sprites['front_default']);
-            $pokemonImage.prependTo($polaroid);
-            $pictureBtn.attr('id', data.name); 
+            $pokemonImage.prependTo($polaroid); 
             $signature.text(data.name)        
     })
 })
@@ -27,15 +25,18 @@ $('form').on('submit', (event) => {
 const closePolaroid = () => {
   $('#modal').css('display', 'none')
 }
+
 const openPolaroid = () => {
     $('#modal').css('display', 'block')
     $('#close-btn').css('display', 'none')
 }
+
 const signature = () => {
     $signature.css('display', 'block')
     $('#signature-btn').css('display', 'none')
     $('#close-btn').css('display', 'flex')   
 }
+
 $('#close-btn').on('click', closePolaroid)
 $('.polaroid-btn').on('click', openPolaroid)
 $('#signature-btn').on('click', signature)
